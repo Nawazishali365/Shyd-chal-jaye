ccb23536f97a454eda69d982d952796bc7aa86c9
Nawazishali365 <117626485+Nawazishali365@users.noreply.github.com>
2024-08-06 07:05:13 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,36 +1,30 @@
-from flask import Flask, jsonify, send_file
+from flask import Flask, render_template
+from flask_cors import CORS
 import requests
 from urllib.request import urlopen
 import json
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS
 
 @app.route('/')
 def index():
-    return send_file('index.html')
-
-@app.route('/get_data')
-def get_data():
-    # Fetch location data
     locURL = 'https://ipinfo.io/json'
     location = urlopen(locURL)
     YourLoc = json.load(location)
     cityName = YourLoc.get('city')
-
-    # Fetch weather data
+    
     apiKey = '98943b1b4b2021db6e7a9f14cb07059b'
     baseURL = 'https://api.openweathermap.org/data/2.5/weather?q='
     completeURL = baseURL + cityName + '&appid=' + apiKey
     response = requests.get(completeURL)
     data = response.json()
-
-    # Extract weather data
+    
     temp = float(data['main']['temp']) - 273.15
     feels_like = float(data['main']['feels_like']) - 273.15
     max_temp = float(data['main']['temp_max']) - 273.15
     min_temp = float(data['main']['temp_min']) - 273.15
 
-    # Extract location data
     location_data = {
         'Your IP': YourLoc.get('ip'),
         'Your City': YourLoc.get('city'),
@@ -42,7 +36,7 @@ def get_data():
         'Your Time Zone': YourLoc.get('timezone')
     }
 
-    return jsonify(temp=temp, feels_like=feels_like, max_temp=max_temp, min_temp=min_temp, location_data=location_data)
+    return render_template('index.html', temp=temp, feels_like=feels_like, max_temp=max_temp, min_temp=min_temp, location_data=location_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
